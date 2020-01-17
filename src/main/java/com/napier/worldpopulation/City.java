d93abee2f213945451259b2fe73797fb60ec095a
oaksoephonekhant <40440090@live.napier.ac.uk>
2020-01-17 14:17:56 +0630

    add Java's comments

--- src/main/java/com/napier/worldpopulation/City.java
+++ src/main/java/com/napier/worldpopulation/City.java
@@ -3,14 +3,32 @@ package com.napier.worldpopulation;
 public class City {
 
     public String name;
+    /**
+     * City's name
+     */
 
     public String country;
-
+    /**
+     *  City's country
+     */
     public String continent;
+    /**
+     * City's Continent
+     */
 
     public String region;
+    /**
+     * City's region
+     */
 
     public String district;
+    /**
+     *
+     * City's District
+     */
 
     public int population;
+    /**
+     * City's population
+     */
 }
--- src/main/java/com/napier/worldpopulation/Country.java
+++ src/main/java/com/napier/worldpopulation/Country.java
@@ -3,14 +3,32 @@ package com.napier.worldpopulation;
 public class Country {
 
     public String code;
+    /**
+     * Country's Code
+     */
 
     public String name;
+    /**
+     * Country's name
+     */
 
     public String continent;
+    /**
+     * Country's Continent
+     */
 
     public String region;
+    /**
+     * Country's region
+     */
 
     public String capital;
+    /**
+     * Country's Capital
+     */
 
     public int population;
+    /**
+     * Country's Population 
+     */
 }

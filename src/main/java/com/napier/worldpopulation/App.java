f342e910c095e00f1a60f71c168272f225bc6177
khanttayza <40440092@live.napier.ac.uk>
2020-01-17 13:30:41 +0630

    queries update

--- src/main/java/com/napier/worldpopulation/App.java
+++ src/main/java/com/napier/worldpopulation/App.java
@@ -76,9 +76,41 @@ public class App
         }
     }
 
+    // Extract all the countries from the database ordered by population
     public ArrayList<Country> getAllCountries() {
         try {
             Statement stmt = con.createStatement();
+            // Region
+            String query = "SELECT * FROM country ORDER BY Population DESC";
+
+            ResultSet rset = stmt.executeQuery(query);
+
+            ArrayList<Country> countries = new ArrayList<Country>();
+
+            while (rset.next()) {
+                Country country = new Country();
+                country.code = rset.getString("Code");
+                country.name = rset.getString("Name");
+                country.capital = rset.getString("Capital");
+                country.region = rset.getString("Region");
+                country.continent = rset.getString("Continent");
+                country.population = rset.getInt("Population");
+                countries.add(country);
+            }
+            return countries;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get countries details");
+            return null;
+        }
+    }
+
+    // Extract all the countries from the database ordered by region and population
+    public ArrayList<Country> getAllCountriesByRegions() {
+        try {
+            Statement stmt = con.createStatement();
+            // Region
             String query = "SELECT * FROM country ORDER BY Region ASC, Population DESC";
 
             ResultSet rset = stmt.executeQuery(query);
@@ -99,7 +131,180 @@ public class App
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
+            System.out.println("Failed to get countries details");
+            return null;
+        }
+    }
+
+    // Extract all the countries from the database ordered by continent and population
+    public ArrayList<Country> getAllCountriesByContinents() {
+        try {
+            Statement stmt = con.createStatement();
+            // Continent
+            String query = "SELECT * FROM country ORDER BY Continent ASC, Population DESC";
+
+            ResultSet rset = stmt.executeQuery(query);
+
+            ArrayList<Country> countries = new ArrayList<Country>();
+
+            while (rset.next()) {
+                Country country = new Country();
+                country.code = rset.getString("Code");
+                country.name = rset.getString("Name");
+                country.capital = rset.getString("Capital");
+                country.region = rset.getString("Region");
+                country.continent = rset.getString("Continent");
+                country.population = rset.getInt("Population");
+                countries.add(country);
+            }
+            return countries;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get countries details");
+            return null;
+        }
+    }
+
+
+    // Extract all the cities from the database ordered by population
+    public ArrayList<City> getAllCities() {
+        try {
+            Statement stmt = con.createStatement();
+            // Continent
+            String query = "SELECT city.Name, city.District, city.Population, country.Name FROM city, country WHERE city.CountryCode=country.Code ORDER BY city.Population DESC";
+
+            ResultSet rset = stmt.executeQuery(query);
+
+            ArrayList<City> cities = new ArrayList<City>();
+
+            while (rset.next()) {
+                City city = new City();
+                city.name = rset.getString("city.Name");
+                city.country = rset.getString("country.Name");
+                city.district = rset.getString("city.District");
+                city.population = rset.getInt("city.Population");
+                cities.add(city);
+            }
+            return cities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities details");
+            return null;
+        }
+    }
+
+    // Extract all the cities from database ordered by continent and population
+    public ArrayList<City> getAllCitiesByContinent() {
+        try {
+            Statement stmt = con.createStatement();
+            // Continent
+            String query = "SELECT city.Name, city.District, city.Population, country.Name, country.Continent FROM city, country WHERE city.CountryCode=country.Code ORDER BY country.Continent ASC, city.Population DESC";
+
+            ResultSet rset = stmt.executeQuery(query);
+
+            ArrayList<City> cities = new ArrayList<City>();
+
+            while (rset.next()) {
+                City city = new City();
+                city.name = rset.getString("city.Name");
+                city.country = rset.getString("country.Name");
+                city.continent = rset.getString("country.Continent");
+                city.district = rset.getString("city.District");
+                city.population = rset.getInt("city.Population");
+                cities.add(city);
+            }
+            return cities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities details");
+            return null;
+        }
+    }
+
+    // Extract all the cities from database ordered by region and population
+    public ArrayList<City> getAllCitiesByRegion() {
+        try {
+            Statement stmt = con.createStatement();
+            // Region
+            String query = "SELECT city.Name, city.District, city.Population, country.Name, country.Region FROM city, country WHERE city.CountryCode=country.Code ORDER BY country.Region ASC, city.Population DESC";
+
+            ResultSet rset = stmt.executeQuery(query);
+
+            ArrayList<City> cities = new ArrayList<City>();
+
+            while (rset.next()) {
+                City city = new City();
+                city.name = rset.getString("city.Name");
+                city.country = rset.getString("country.Name");
+                city.region = rset.getString("country.Region");
+                city.district = rset.getString("city.District");
+                city.population = rset.getInt("city.Population");
+                cities.add(city);
+            }
+            return cities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities details");
+            return null;
+        }
+    }
+
+    // Extract all the cities from database ordered by district and population
+    public ArrayList<City> getAllCitiesByDistrict() {
+        try {
+            Statement stmt = con.createStatement();
+            // District
+            String query = "SELECT city.Name, city.District, city.Population, city.District, country.Name FROM city, country WHERE city.CountryCode=country.Code ORDER BY city.District ASC, city.Population DESC";
+
+            ResultSet rset = stmt.executeQuery(query);
+
+            ArrayList<City> cities = new ArrayList<City>();
+
+            while (rset.next()) {
+                City city = new City();
+                city.name = rset.getString("city.Name");
+                city.country = rset.getString("country.Name");
+                city.district = rset.getString("city.District");
+                city.population = rset.getInt("city.Population");
+                cities.add(city);
+            }
+            return cities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities details");
+            return null;
+        }
+    }
+
+    // Extract all the countries from database ordered by countries
+    public ArrayList<City> getAllCitiesByCountries() {
+        try {
+            Statement stmt = con.createStatement();
+            // Continent
+            String query = "SELECT city.Name, city.District, city.Population, city.District, country.Name FROM city, country WHERE city.CountryCode=country.Code ORDER BY country.Name ASC, city.Population DESC";
+
+            ResultSet rset = stmt.executeQuery(query);
+
+            ArrayList<City> cities = new ArrayList<City>();
+
+            while (rset.next()) {
+                City city = new City();
+                city.name = rset.getString("city.Name");
+                city.country = rset.getString("country.Name");
+                city.district = rset.getString("city.District");
+                city.population = rset.getInt("city.Population");
+                cities.add(city);
+            }
+            return cities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities details");
             return null;
         }
     }
@@ -113,13 +318,15 @@ public class App
         a.connect();
 
         // Extract countries
-        ArrayList<Country> countries = a.getAllCountries();
+        ArrayList<Country> countries = a.getAllCountriesByRegions();
+        ArrayList<City> cities = a.getAllCitiesByContinent();
 
         System.out.println(countries.size());
+        System.out.println(cities.size());
         // Print countries
-        for (int i = 0; i < countries.size(); i++) {
-            Country country = countries.get(i);
-            System.out.println(country.name + ", " + Integer.toString(country.population) + ", " + country.region);
+        for (int i = 0; i < cities.size(); i++) {
+            City city = cities.get(i);
+            System.out.println(city.name + ", " + Integer.toString(city.population) + ", " + city.continent);
         }
 
         // Disconnect from database

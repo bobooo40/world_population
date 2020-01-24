668cabc6234c798b8e9ae0137f289ad9379b6c0f
shoonlaiyee <40440094@live.napier.ac.uk>
2020-01-24 12:54:29 +0630

    Integration testing (fix)

--- src/main/java/com/napier/worldpopulation/App.java
+++ src/main/java/com/napier/worldpopulation/App.java
@@ -14,7 +14,7 @@ public class App
         try
         {
             // Load Database driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
         {
@@ -34,7 +34,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://"+location+"/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?" + "allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
@@ -155,7 +155,14 @@ public class App
         App a = new App();
 
         // Connect to database
-        a.connect("localhost:33060");
+        if (args.length < 1)
+        {
+            a.connect("localhost:3306");
+        }
+        else
+        {
+            a.connect(args[0]);
+        }
 
         // Countries Report Generation
         ArrayList<City> capitals = a.capitals(4);

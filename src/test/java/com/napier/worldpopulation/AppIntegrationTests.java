f048b93fb1d336eb77901401c3aa30c2c67728c4
Bo Bo Oo <40437537@live.napier.ac.uk>
2020-01-24 12:26:21 +0630

    Add Integration Tests

--- src/test/java/com/napier/worldpopulation/AppIntegrationTests.java
+++ src/test/java/com/napier/worldpopulation/AppIntegrationTests.java
@@ -1,4 +1,28 @@
 package com.napier.worldpopulation;
 
-public class AppIntegrationTests {
+import com.napier.worldpopulation.App;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTests
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060");
+    }
+
+    @Test
+    void testGetCities()
+    {
+
+    }
 }
